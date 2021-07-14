d3f7fdc966801a6f4e7c6fa9c3e0a036a8708d3c
Milen Penev <milennpenev@gmail.com>
2021-07-14 15:06:33 +0300

    fix

--- Classes and Objects/04.pizza_delivery.py
+++ Classes and Objects/04.pizza_delivery.py
@@ -23,6 +23,7 @@ class PizzaDelivery:
                     return f"Please check again the desired quantity of {ingredient}!"
                 self.ingredients[ingredient] -= quantity
                 self.price -= price_per_ingredient * quantity
+            return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
         else:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
 
@@ -30,8 +31,25 @@ class PizzaDelivery:
         if self.ordered:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
         else:
-            return f"You've ordered pizza {self.name} prepared with {f'{key}:{value}' for key,value in self.ingredients} and the price will be {self.price}lv."
+            result = ''
+            self.ordered = True
+            index = 0
+            for k, v in self.ingredients.items():
+                index += 1
+                if index == len(self.ingredients):
+                    result += f"{k}: {v} "
+                else:
+                    result += f"{k}: {v}, "
+            return f"You've ordered pizza {self.name} prepared with {result}and the price will be {self.price}lv."
 
 
 margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
 margarita.add_extra('mozzarella', 1, 0.5)
+margarita.add_extra('cheese', 1, 1)
+margarita.remove_ingredient('cheese', 1, 1)
+print(margarita.remove_ingredient('bacon', 1, 2.5))
+print(margarita.remove_ingredient('tomatoes', 2, 0.5))
+margarita.remove_ingredient('cheese', 2, 1)
+print(margarita.make_order())
+print(margarita.add_extra('cheese', 1, 1))
+
