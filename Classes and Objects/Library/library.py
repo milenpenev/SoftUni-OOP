6c354246524b5ebf2079d8579184592cf1c5fc52
Milen Penev <milennpenev@gmail.com>
2021-07-15 17:04:45 +0300

    exercise

---
+++ Classes and Objects/Library/library.py
@@ -0,0 +1,31 @@
+from user import User
+
+class Library:
+    def __init__(self):
+        self.user_records = []
+        self.books_available = {}
+        self.rented_books = {}
+
+    def add_user(self, user: User):
+        if user not in self.user_records:
+            self.user_records.append(user)
+        else:
+            return f"User with id = {user.id} already registered in the library!"
+
+    def remove_user(self, user: User):
+        if user not in self.user_records:
+            self.user_records.remove(user)
+        else:
+            return "We could not find such user to remove!"
+
+    def change_username(self, user_id, new_username):
+        for user in self.user_records:
+            if user.id == id and user.username != new_username:
+                if user.username in self.rented_books:
+                    self.rented_books[new_username] = self.rented_books[user.username]
+                    del(self.rented_books[user.username])
+                user.username = new_username
+                return f"Username successfully changed to: {new_username} for userid: {user_id}"
+            else:
+                return "Please check again the provided username - it should be different than the username used so far!"
+        return f"There is no user with id = {user_id}!"
---
+++ Classes and Objects/Library/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, user_id, username):
+        self.user_id = user_id
+        self.username = username
+        self.books = []
+
+    def get_book(self, author, book_name, days_to_return, library):
+        pass
