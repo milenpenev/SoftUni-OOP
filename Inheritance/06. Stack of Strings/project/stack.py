f0922959c0f6ef77e5665d0fb3c58f5909f854f5
Milen Penev <milennpenev@gmail.com>
2021-07-21 13:30:26 +0300

    Lecture

---
+++ Inheritance/06. Stack of Strings/project/stack.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Stack:
+    def __init__(self):
+        self.data: List[str] = []
+
+    def push(self, element):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        if self.data:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return '[' + ', '.join(reversed(self.data)) + ']'
