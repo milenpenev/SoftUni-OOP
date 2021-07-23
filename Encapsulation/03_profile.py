6bbdc33e00039b6a2f430cd651ef20b694e4c370
Milen Penev <milennpenev@gmail.com>
2021-07-23 17:25:33 +0300

    Leture

--- Encapsulation/03_profile.py
+++ Encapsulation/03_profile.py
@@ -8,8 +8,35 @@ class Profile:
         return self.__username
 
     @username.setter
-    def username(self, username):
-        if 5 > len(username) > 15:
+    def username(self, value):
+        if 5 > len(value) or len(value) > 15:
             raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
 
-profile_with_invalid_username = Profile('Too_long_usernameeee', 'Any')
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if self.is_length_valid(value) and self.contains_uppercase(value) and self.contains_number(value):
+            self.__password = value
+            return
+        raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+    def is_length_valid(self, password):
+        return len(password) >= 8
+
+    def contains_uppercase(self, password):
+        upper_letters = [char for char in password if char.isupper()]
+        return True if upper_letters else False
+
+    def contains_number(self, password):
+        nums = [char for char in password if char.isdigit()]
+        return True if nums else False
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*".join(["" for el in range(len(self.password)+1)])}'
+
+correct_profile = Profile("Username", "Passw0rd")
+print(correct_profile)
