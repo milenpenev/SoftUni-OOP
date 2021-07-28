2490e0c61c0e454f9b1378ed7a566d70983241cc
Milen Penev <milennpenev@gmail.com>
2021-07-28 17:34:20 +0300

    Lecture and exercise

--- Encapsulation/01_wild_cat_zoo/project/zoo.py
+++ Encapsulation/01_wild_cat_zoo/project/zoo.py
@@ -15,3 +15,39 @@ class Zoo:
         if len(self.animals) < self.__animal_capacity and self.__budget < price:
             return "Not enough budget"
         return "Not enough space for animal"
+
+    def hire_worker(self, worker):
+        if len(self.workers) < self.__workers_capacity:
+            self.workers.append(worker)
+            return f"{worker.name} the {type(worker).__class__} hired successfully"
+        return "Not enough space for worker"
+
+    def fire_worker(self, worker_name):
+        for worker in self.workers:
+            if worker.name == worker_name:
+                self.workers.remove(worker_name)
+                return f"{worker_name} fired successfully"
+        return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        total_salaries = 0
+        for worker in self.workers:
+            total_salaries += worker.salary
+
+        if total_salaries <= self.__budget:
+            self.__budget -= total_salaries
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        total_money_for_care = sum(map(lambda animal: animal.money_for_care, self.animals))
+        if total_money_for_care <= self.__budget:
+            self.__budget -= total_money_for_care
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self, amount):
+        self.__budget += amount
+
+    def animals_status(self):
+        result = f'You have {sum(self.animals)} animals'
