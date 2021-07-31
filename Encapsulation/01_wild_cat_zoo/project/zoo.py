15f71fe84b04b2ae0fccd760650ea209540dfc0b
Milen Penev <milennpenev@gmail.com>
2021-07-31 16:29:34 +0300

    Lecture and exercise

--- Encapsulation/01_wild_cat_zoo/project/zoo.py
+++ Encapsulation/01_wild_cat_zoo/project/zoo.py
@@ -1,3 +1,11 @@
+from project.tiger import Tiger
+from project.cheetah import Cheetah
+from project.lion import Lion
+from project.keeper import Keeper
+from project.caretaker import Caretaker
+from project.vet import Vet
+
+
 class Zoo:
     def __init__(self, name, budget, animal_capacity, workers_capacity):
         self.name = name
@@ -11,7 +19,7 @@ class Zoo:
         if len(self.animals) < self.__animal_capacity and self.__budget >= price:
             self.animals.append(animal)
             self.__budget -= price
-            return f"{animal.name} the {type(animal).__class__} added to the zoo"
+            return f"{animal.name} the {type(animal).__name__} added to the zoo"
         if len(self.animals) < self.__animal_capacity and self.__budget < price:
             return "Not enough budget"
         return "Not enough space for animal"
@@ -19,13 +27,13 @@ class Zoo:
     def hire_worker(self, worker):
         if len(self.workers) < self.__workers_capacity:
             self.workers.append(worker)
-            return f"{worker.name} the {type(worker).__class__} hired successfully"
+            return f"{worker.name} the {type(worker).__name__} hired successfully"
         return "Not enough space for worker"
 
     def fire_worker(self, worker_name):
         for worker in self.workers:
             if worker.name == worker_name:
-                self.workers.remove(worker_name)
+                self.workers.remove(worker)
                 return f"{worker_name} fired successfully"
         return f"There is no {worker_name} in the zoo"
 
@@ -50,4 +58,59 @@ class Zoo:
         self.__budget += amount
 
     def animals_status(self):
-        result = f'You have {sum(self.animals)} animals'
+        result = f'You have {len(self.animals)} animals\n'
+        total_lions = 0
+        lions_info = []
+        total_tigers = 0
+        tigers_info = []
+        total_cheetahs = 0
+        cheetahs_info = []
+        for animal in self.animals:
+            if type(animal) == Lion:
+                total_lions += 1
+                lions_info.append(repr(animal))
+            elif type(animal) == Tiger:
+                total_tigers += 1
+                tigers_info.append(repr(animal))
+            elif type(animal) == Cheetah:
+                total_cheetahs += 1
+                cheetahs_info.append(repr(animal))
+        result += f"----- {total_lions} Lions:\n"
+        lions_result = '\n'.join(lions_info)
+        result += lions_result + '\n'
+        result += f'----- {total_tigers} Tigers:\n'
+        tigers_result = '\n'.join(tigers_info)
+        result += tigers_result + '\n'
+        result += f'----- {total_cheetahs} Cheetahs:\n'
+        cheetahs_result = '\n'.join(cheetahs_info)
+        result += cheetahs_result
+        return result
+
+    def workers_status(self):
+        result = f'You have {len(self.workers)} workers\n'
+        total_keepers = 0
+        keepers_info = []
+        total_caretakers = 0
+        caretakers_info = []
+        total_vets = 0
+        vets_info = []
+        for worker in self.workers:
+            if type(worker) == Keeper:
+                total_keepers += 1
+                keepers_info.append(repr(worker))
+            elif type(worker) == Caretaker:
+                total_caretakers += 1
+                caretakers_info.append(repr(worker))
+            elif type(worker) == Vet:
+                total_vets += 1
+                vets_info.append(repr(worker))
+        result += f"----- {total_keepers} Keepers:\n"
+        keepers_result = '\n'.join(keepers_info)
+        result += keepers_result + '\n'
+        result += f'----- {total_caretakers} Caretakers:\n'
+        caretakers_result = '\n'.join(caretakers_info)
+        result += caretakers_result + '\n'
+        result += f'----- {total_vets} Vets:\n'
+        vets_result = '\n'.join(vets_info)
+        result += vets_result
+        return result
