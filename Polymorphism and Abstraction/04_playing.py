f3547733d94822c4861512997bc3841236c9c7c9
Milen Penev <milennpenev@gmail.com>
2021-08-14 18:43:31 +0300

    Exercise and lecture

---
+++ Polymorphism and Abstraction/04_playing.py
@@ -0,0 +1,9 @@
+def start_playing(instance):
+    return instance.play()
+
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+guitar = Guitar()
+print(start_playing(guitar))
